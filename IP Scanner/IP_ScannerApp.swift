ca3b7c9bc5811f98dc8ddc673281f8597a3b57e8
George Babichev <gbabichev@xantrion.com>
2026-01-02 12:12:20 -0500

    Initial Commit

---
+++ IP Scanner/IP_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IP_ScannerApp.swift
+//  IP Scanner
+//
+//  Created by George Babichev on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct IP_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
